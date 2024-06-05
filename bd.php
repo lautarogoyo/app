8a815f8170caeccf7dc4e74775eaa02b0c391a9d
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2024-06-05 12:41:54 -0300

    Add files via upload

---
+++ bd.php
@@ -0,0 +1,13 @@
+<?php
+
+$servidor="localhost"; // 127.0.0.1 
+$baseDeDatos="app";
+$usuario="root";
+$contraseña="";
+try{
+    $conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$contraseña);
+} catch(Exception $ex){
+    echo $ex->getMessage();
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php include("templates/header.php"); ?>
+</br>
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Bienvenido al sistema <?php echo $_SESSION['usuario']?></h1>
+        <h4>Solamente tienes acceso a Proveedores y Obras</h4>
+    </div>
+</div>
+<?php include("templates/footer.php"); ?>
+        
